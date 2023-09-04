493a0f60a7f9d94452efa1410644d3e3fe274136
이지현 <98106371+easyhyun00@users.noreply.github.com>
2023-09-04 15:11:13 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,9 @@
+function solution(x) {
+    var sum = 0;
+    var num = x;
+    while (num > 0) {
+        sum += num % 10;
+        num = Math.floor(num/10);
+    }
+    return x % sum == 0 ? true : false;
+}
