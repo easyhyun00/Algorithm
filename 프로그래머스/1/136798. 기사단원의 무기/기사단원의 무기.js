c4624773161d9c686b868e101ea684de37d5790c
이지현 <leejihyun0324@gmail.com>
2024-07-15 18:04:18 +0900

    [level 1] Title: 기사단원의 무기, Time: 116.13 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,15 +1,17 @@
 function solution(number, limit, power) {
-    const arr = []
+    let arr = []
     for(i=1; i<=number; i++) {
         arr.push(i);
     }
-    return arr.reduce((acc, cur)=>{
-       let cnt = 0;
-        for(i = 1; i <= Math.sqrt(cur); i++) {
+    const result = arr.reduce((acc,cur)=>{
+        let cnt = 0;
+        for(i=1; i<=Math.sqrt(cur); i++) {
             if (cur % i === 0) {
-                cnt += (i === cur / i) ? 1 : 2;
+                if (i === Math.sqrt(cur)) cnt+=1;
+                else cnt+=2;
             }
         }
-        return acc += cnt > limit ? power : cnt; 
+        return acc += cnt > limit ? power : cnt;
     },0)
+    return result;
 }
