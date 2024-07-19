0467854e9769ae3a3feccb7cca6394f3c61bd36e
이지현 <leejihyun0324@gmail.com>
2024-07-19 21:29:33 +0900

    [level 1] Title: 폰켓몬, Time: 1.95 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,9 @@
 function solution(nums) {
-    const newSet = new Set(nums);
-    const result = [...newSet].length;
-    const count = nums.length / 2
-    return result < count ? result : count;
+    const table = new Map();
+    
+    nums.map((item)=>{
+        table.set(item, true);
+    })
+    
+    return Math.min(nums.length/2, table.size);
 }
