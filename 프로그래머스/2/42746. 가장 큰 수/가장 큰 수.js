782edd7b79b4c1dd642fa9a64bccbc636c2835a2
이지현 <leejihyun0324@gmail.com>
2024-07-22 18:56:14 +0900

    [level 2] Title: 가장 큰 수, Time: 127.12 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,7 @@
 function solution(numbers) {
     numbers = numbers.map((item)=>item.toString());
     
-    numbers.sort((a,b)=> (b+a) > (a+b) ? 1 : -1);
-    
-    return numbers.join('')[0] === '0' ? '0' : numbers.join('');
+    const result = numbers.sort((a,b) => (a+b) >= (b+a) ? -1 : 1).join('');
+
+    return result[0] === '0' ? '0' : result;
 }
