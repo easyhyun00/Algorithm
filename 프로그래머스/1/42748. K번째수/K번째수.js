a52fa63f9c3506486ff8c158b818ff2fe1d66853
이지현 <leejihyun0324@gmail.com>
2024-07-22 14:12:20 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,6 +1,5 @@
 function solution(array, commands) {
-    const result = commands.map((item)=>{
-        return array.slice(item[0] - 1, item[1]).sort((a,b)=>a-b)[item[2] -1 ];
+    return commands.map((item)=>{
+        return array.slice(item[0]-1,item[1]).sort((a,b)=>a-b)[item[2]-1]
     })
-    return result;
 }
