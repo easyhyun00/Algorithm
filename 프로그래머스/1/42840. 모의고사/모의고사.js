b96042a1b566a053ffa831616bb3aea8c51d32cb
이지현 <leejihyun0324@gmail.com>
2024-07-23 10:57:40 +0900

    [level 1] Title: 모의고사, Time: 2.00 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,16 +3,18 @@ function solution(answers) {
     const two = [2,1,2,3,2,4,2,5];
     const three = [3,3,1,1,2,2,4,4,5,5];
     
-    let cnt = [0, 0, 0]
-    answers.map((item, index)=>{
-        if (item === one[index % one.length]) cnt[0] += 1
-        if (item === two[index % two.length]) cnt[1] += 1
-        if (item === three[index % three.length]) cnt[2] += 1
+    let result = [0,0,0];
+    answers.map((item,index)=>{
+        item === one[index % one.length] && result[0]++
+        item === two[index % two.length] && result[1]++
+        item === three[index % three.length] && result[2]++
     })
-    const big = Math.max(...cnt);
+    
     let answer = [];
-    for(i=0; i<cnt.length; i++) {
-        if (cnt[i] === big) answer.push(i + 1);
-    }
+    const cnt = Math.max(...result);
+    result.map((item,index)=>{
+        item === cnt && answer.push(index+1);
+    })
+    
     return answer;
 }
