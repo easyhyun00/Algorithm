6afbaf803f2175f4a161512132ad40e6639acbd0
이지현 <leejihyun0324@gmail.com>
2024-08-03 15:13:23 +0900

    [level 2] Title: 소수 찾기, Time: 5.33 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,30 @@
+function solution(numbers) {
+    const result = new Set();
+    const isVisited = new Array(numbers.length).fill(false);
+    
+    // 탐색 함수
+    const dfs = (num) => {
+        if (num.length > 0) result.add(Number(num));
+
+        for(let i=0; i<numbers.length; i++) {
+            if(!isVisited[i]) {
+                isVisited[i] = true;
+                dfs(num + numbers[i]);
+                isVisited[i] = false;
+            }
+        }
+    }
+    
+    dfs('');
+      
+    // 소수판별 함수
+    const isFrime = (num) => {
+        if (num < 2) return false;
+        for(j=2; j<=Math.sqrt(num); j++) {
+            if (num % j === 0) return false;
+        }
+        return true;
+    }
+    
+    return [...result].filter((item)=> isFrime(item)).length;
+}
