08edf37e92e3962018833152bb13d1e3a1fc5135
이지현 <leejihyun0324@gmail.com>
2024-08-03 15:32:21 +0900

    [level 2] Title: 피로도, Time: 7.06 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,43 +1,21 @@
-// function solution(k, dungeons) {
-//     let answer = [];
-//     let visited = Array(dungeons.length).fill(false);
-
-//     const dfs = (count, hp) => {
-//         answer.push(count);
-        
-//         for (let i = 0; i < dungeons.length; i++) {
-//             if (!visited[i] && dungeons[i][1] <= hp) {
-//                 visited[i] = true;
-//                 dfs(count + 1, hp - dungeons[i][1]);
-//                 visited[i] = false;
-//             }
-//         }
-//     }
-
-//     dfs(0, k);
-    
-//     return Math.max(...answer);
-// }
-
 function solution(k, dungeons) {
-    let answer = 0;
+    let answer = [];
     let visited = new Array(dungeons.length).fill(false);
 
-
-    function DFS(k, count) {
+    const dfs = (k, count) => {
+        
+        answer.push(count);
 
         for(let i=0; i<dungeons.length; i++) {
             if(k >= dungeons[i][0] && !visited[i]) {
                 visited[i] = true;
-                DFS(k-dungeons[i][1], count + 1);
+                dfs(k-dungeons[i][1], count + 1);
                 visited[i] = false; 
             }
         }
-
-        answer = Math.max(answer, count);
     }
 
-    DFS(k, 0);
-
-    return answer;
+    dfs(k, 0);
+    
+    return Math.max(...answer);
 }
