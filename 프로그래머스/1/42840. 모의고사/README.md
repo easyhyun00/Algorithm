3bf090ed64b9869c60e4063c196991efc4169259
이지현 <leejihyun0324@gmail.com>
2024-11-11 23:56:03 +0900

    [level 1] Title: 모의고사, Time: 2.43 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.8 MB, 시간: 2.00 ms
+메모리: 36.9 MB, 시간: 2.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 23일 10:57:39
+2024년 11월 11일 23:56:02
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,20 +1,20 @@
 function solution(answers) {
-    const one = [1,2,3,4,5];
-    const two = [2,1,2,3,2,4,2,5];
-    const three = [3,3,1,1,2,2,4,4,5,5];
+    const result = [];
+    const supo1 = [1,2,3,4,5];
+    const supo2 = [2,1,2,3,2,4,2,5];
+    const supo3 = [3,3,1,1,2,2,4,4,5,5];
     
-    let result = [0,0,0];
+    const count = [0,0,0];
+    
     answers.map((item,index)=>{
-        item === one[index % one.length] && result[0]++
-        item === two[index % two.length] && result[1]++
-        item === three[index % three.length] && result[2]++
+        if(supo1[index%5] === item) count[0] += 1;
+        if(supo2[index%8] === item) count[1] += 1;
+        if(supo3[index%10] === item) count[2] += 1;
     })
     
-    let answer = [];
-    const cnt = Math.max(...result);
-    result.map((item,index)=>{
-        item === cnt && answer.push(index+1);
+    const max = Math.max(...count);
+    count.map((item,index)=>{
+        if(item === max) result.push(index+1)
     })
-    
-    return answer;
+    return result;
 }
