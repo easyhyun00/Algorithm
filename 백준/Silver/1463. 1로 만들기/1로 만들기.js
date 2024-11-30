5dbcd03b4e10598630e4e992b7be9d44c2907b3a
이지현 <leejihyun0324@gmail.com>
2024-11-30 20:55:52 +0900

    [Silver III] Title: 1로 만들기, Time: 168 ms, Memory: 17456 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,17 +1,24 @@
-const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+const input = Number(
+  require('fs').readFileSync('/dev/stdin').toString().trim()
+);
 
-let arr = Array.from(Array(input + 1).fill(0));
+const dp = new Array(input + 1).fill(0);
 
 for (let i = 2; i <= input; i++) {
-  arr[i] = arr[i - 1] + 1;
+  // 1을 빼는 방법
+  dp[i] = dp[i - 1] + 1;
 
+  // 2로 나누는 방법
   if (i % 2 === 0) {
-    arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
   }
 
+  // 3으로 나누는 방법
   if (i % 3 === 0) {
-    arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
   }
+
+  // 이번 방법에다가 +1한 값 중에 가장 작은 값
 }
 
-console.log(arr[input]);
+console.log(dp[input]);
