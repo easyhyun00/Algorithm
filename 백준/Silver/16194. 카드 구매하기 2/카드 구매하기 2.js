4f39a31575de96d0834f476e87e236789f43c05b
이지현 <leejihyun0324@gmail.com>
2024-12-12 22:15:54 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 160 ms, Memory: 9984 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.js
@@ -0,0 +1,19 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const cardList = [0, ...arr];
+const dp = new Array(N + 1).fill(0);
+
+for (let i = 1; i <= N; i++) {
+  dp[i] = cardList[i];
+  for (let j = 0; j < i; j++) {
+    dp[i] = Math.min(dp[i], dp[j] + cardList[i - j]);
+  }
+}
+
+console.log(dp[N]);
