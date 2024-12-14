1b9cf0974ceceb58044d89643224e525777b798d
이지현 <leejihyun0324@gmail.com>
2024-12-14 23:19:08 +0900

    [Silver III] Title: 이친수, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.js
+++ 백준/Silver/2193. 이친수/이친수.js
@@ -2,16 +2,14 @@ const input = Number(
   require('fs').readFileSync('/dev/stdin').toString().trim()
 );
 
-const dp = new Array(input + 1);
+const dp = Array.from({ length: input + 1 }, () => Array(2).fill(BigInt(0)));
 /**
  * 0으로 시작 X
  * 1연속 X
  */
-dp[0] = [0, 0];
-dp[1] = [0, 1];
+dp[1] = [BigInt(0), BigInt(1)];
 
 for (let i = 2; i <= input; i++) {
-  dp[i] = [];
   dp[i][0] = BigInt(dp[i - 1][0] + dp[i - 1][1]);
   dp[i][1] = BigInt(dp[i - 1][0]);
 }
