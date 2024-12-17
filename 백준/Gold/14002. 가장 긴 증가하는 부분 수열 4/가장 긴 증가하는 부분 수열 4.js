eac77128e7447864cb38fc5b89539ebaef3c37ba
이지현 <leejihyun0324@gmail.com>
2024-12-18 00:27:04 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 180 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.js
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.js
@@ -7,7 +7,7 @@ const input = require('fs')
 const N = Number(input[0]);
 const arr = input[1].split(' ').map(Number);
 
-const map = Array.from({ length: N }, () => Array.from(N).fill(0));
+const map = Array.from({ length: N }, () => []);
 map[0].push(arr[0]);
 
 for (let i = 1; i < N; i++) {
@@ -21,15 +21,13 @@ for (let i = 1; i < N; i++) {
   }
 }
 
-let index = 0;
-let result = 0;
+let result = [];
 
 for (let i = 0; i < N; i++) {
-  if (map[i].length > result) {
-    result = map[i].length;
-    index = i;
+  if (result.length < map[i].length) {
+    result = map[i];
   }
 }
 
-console.log(result);
-console.log(map[index].join(' '));
+console.log(result.length);
+console.log(result.join(' '));
