d62a69d049280ab7c242a0280ff9ed90d3899f9e
이지현 <leejihyun0324@gmail.com>
2024-12-17 23:26:11 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 160 ms, Memory: 9664 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -1,25 +1,18 @@
 const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .readFileSync('/dev/stdin')
   .toString()
   .trim()
-  .split('\n')
-  .map((el) => el.split(' ').map(Number));
+  .split('\n');
 
-const N = input.shift()[0];
-const arrA = input[0];
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
 
-const dp = new Array(N).fill(0);
+const dp = new Array(N).fill(1);
 
-for (let i = 0; i < N; i++) {
-  // 나로 시작
-  dp[i] = 1;
-
-  // 이전 결과 반복문
+for (let i = 1; i < N; i++) {
   for (let j = 0; j < i; j++) {
-    // 내가 이전 결과보다 크고, 이전 결과에 +1한 것보다 작으면
-    if (arrA[i] > arrA[j] && dp[i] < dp[j] + 1) {
-      // 제일 큰 값으로 업데이트
-      dp[i] = dp[j] + 1;
+    if (arr[i] > arr[j]) {
+      dp[i] = Math.max(dp[j] + 1, dp[i]);
     }
   }
 }
