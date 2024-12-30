4716e3213159a446039dd3ab17cefd2ccfc17172
이지현 <leejihyun0324@gmail.com>
2024-12-30 16:13:04 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,16 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+const [A, B, C] = input;
+
+console.log((A + B) % C);
+
+console.log(((A % C) + (B % C)) % C);
+
+console.log((A * B) % C);
+
+console.log(((A % C) * (B % C)) % C);
