a3ed45045033bec79c201cad99e0c5831fc2b0ab
이지현 <leejihyun0324@gmail.com>
2024-12-30 17:09:52 +0900

    [Gold IV] Title: 약수의 합, Time: 328 ms, Memory: 58836 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.js
+++ 백준/Gold/17425. 약수의 합/약수의 합.js
@@ -8,25 +8,39 @@ const input = require('fs')
 const arr = input.slice(1);
 let result = [];
 
-// 약수 합
+// [1] 약수 합
 const max = Math.max(...arr);
 const divisor = new Array(max + 1).fill(0);
 
+/**
+ * i = 2 일 때,
+ * divisor[2], [4], [6], [8], ..., [10000] 에다가
+ * i 값을 더함.
+ *
+ * 그러면 divisor[j]에는 모든 약수들의 합이 들어가게 됨.
+ */
 for (let i = 1; i <= max; i++) {
   for (let j = i; j <= max; j += i) {
     divisor[j] += i;
   }
 }
 
-// 누적 합
-const cumulative = new Array(max + 1).fill(0);
+// [2] 누적 합
+const sum = new Array(max + 1).fill(0);
 
+/**
+ * 이전 누적합 sum[i-1]에서
+ * 현재 수의 약수 합을 더해,
+ * sum[i]의 약수 합을 구함
+ */
 for (let i = 1; i <= max; i++) {
-  cumulative[i] = cumulative[i - 1] + divisor[i];
+  sum[i] = sum[i - 1] + divisor[i];
 }
 
+// [3] 특정 수까지의 누적합을 결과에 넣음
 arr.forEach((item) => {
-  result.push(cumulative[item]);
+  result.push(sum[item]);
 });
 
+// 출력
 console.log(result.join('\n'));
