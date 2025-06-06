50f360d43a3b0028035345afa58db96f821e9a71
이지현 <leejihyun0324@gmail.com>
2025-06-06 23:56:00 +0900

    [level 1] Title: 카드 뭉치, Time: 0.13 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,15 @@
 function solution(cards1, cards2, goal) {
-    let secondIdx = 0;
-    for(i=0; i<goal.length; i++) {
-        if (goal[i] === cards1[i - secondIdx]) continue;
-        else {
-            if (goal[i] === cards2[secondIdx]) {
-                secondIdx += 1;
-                continue;
-            } else return "No";
+    let result = 'Yes';
+    
+    for(let i=0; i<goal.length; i++) {
+        if(cards1[0] === goal[i]) {
+            cards1.shift();
+        } else if (cards2[0] === goal[i]) {
+            cards2.shift();
+        } else {
+            result = "No";
+            break;
         }
     }
-    return "Yes";
+    return result;
 }
