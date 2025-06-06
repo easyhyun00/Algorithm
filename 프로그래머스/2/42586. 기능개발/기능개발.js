671e8c9caa11d42346917e53ab29232be399dbb3
이지현 <leejihyun0324@gmail.com>
2025-06-06 23:12:04 +0900

    [level 2] Title: 기능개발, Time: 0.36 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,30 +1,19 @@
 function solution(progresses, speeds) {
-    let arr = [];
+    const result = [];
     
-    for(j=0; progresses.length !== 0; j++) {
-        console.log(progresses);
-         while (progresses[0] < 100) {
-            for(i=0; i<progresses.length; i++) {
-                progresses[i] += speeds[i]
-            }
+    while(progresses.length) {
+        for(let i=0; i<progresses.length; i++) {
+            progresses[i] += speeds[i]
         }
         
-        console.log(progresses);
-       
-        let cnt = 0;
-        
-        for(k=0; progresses.length !== 0; k++) {
-            if(progresses[k]>=100) {
-                cnt++;
-                progresses.splice(k, 1);
-                speeds.splice(k,1);
-                k--;
-            } else break;
+        let count = 0;
+        while(progresses.length && progresses[0] >= 100) {
+            count ++;
+            progresses.shift();
+            speeds.shift();
         }
+        count && result.push(count)
         
-        arr.push(cnt);
-        console.log(progresses);
-        console.log();
     }
-    return arr;
+    return result;
 }
