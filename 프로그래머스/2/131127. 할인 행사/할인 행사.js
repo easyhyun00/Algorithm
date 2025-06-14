ae1b174e4b933c86e10355bd3cc79b018df17402
이지현 <leejihyun0324@gmail.com>
2025-06-14 10:10:31 +0900

    [level 2] Title: 할인 행사, Time: 494.16 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,40 +1,4 @@
-/**
- * 할인 행사
- * [https://school.programmers.co.kr/learn/courses/30/lessons/131127]
- */
-
-const checkDiscount = (wantHash, discountHash) => {
-  if (Object.keys(wantHash).length !== Object.keys(discountHash).length)
-    return false;
-
-  for (const key in wantHash) {
-    if (discountHash[key] !== wantHash[key]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const solution = (want, number, discount) => {
-  // 사고 싶은 것
-  const wantHash = {};
-  for (let i = 0; i < want.length; i++) {
-    wantHash[want[i]] = number[i];
-  }
-
-  let result = 0;
-
-  // 특정일 가입 시 할인 받을 수 있는 것
-  for (let i = 0; i < discount.length - 9; i++) {
-    const discountHash = {};
-    for (let j = i; j < i + 10; j++) {
-      if (discountHash[discount[j]]) discountHash[discount[j]] += 1;
-      else discountHash[discount[j]] = 1;
-    }
-
-    // 할인하는 상품 개수와 원하는게 일치하면 + 1
-    if (checkDiscount(wantHash, discountHash)) result += 1;
-  }
-
-  return result;
-};
+function solution(want, number, discount) {
+    var answer = 0;
+    return answer;
+}
