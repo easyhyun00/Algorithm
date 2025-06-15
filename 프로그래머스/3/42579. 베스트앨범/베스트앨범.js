3815e03e47d12e1879f33cfe0bd72035e4df6a29
이지현 <leejihyun0324@gmail.com>
2025-06-15 22:27:33 +0900

    [level 3] Title: 베스트앨범, Time: 0.27 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,24 +1,45 @@
-function solution(genres, plays) {
-    const table = new Map();
-    const result = [];
-    
-    const songs = genres.map((item, index)=>{
-        table.set(item, (table.get(item) || 0) + plays[index])
-        return [item, plays[index], index]
-    }).sort(([, a,], [, b,]) => b-a);
-    
-    const order = [...table].sort(([, a], [, b]) => b-a);
-    
-    order.map((item)=>{
-        let cnt = 0;
-        for(const music of songs) {
-            if(music[0] === item[0]) {
-                cnt ++;
-                result.push(music[2]);
-            } 
-            if (cnt === 2) break;
-        }
-    })
-    
-    return result;
-}
+/**
+ * 베스트 앨범
+ * [https://school.programmers.co.kr/learn/courses/30/lessons/42579]
+ */
+
+const solution = (genres, plays) => {
+  const result = [];
+  const gHash = {}; // 장르 별 재생 횟수 저장
+  const pHash = {}; // 각 곡 재생 횟수 저장
+
+  genres.forEach((genre, index) => {
+    gHash[genre] = (gHash[genre] || 0) + plays[index];
+    pHash[index] = { genre, play: plays[index] };
+  });
+
+  // 장르 내림차순 정렬
+  const sortedGenres = Object.entries(gHash).sort((a, b) => b[1] - a[1]);
+  // 각 곡 내림차순 정렬
+  const sortedPlays = Object.entries(pHash).sort(
+    (a, b) => b[1].play - a[1].play
+  );
+
+  sortedGenres.forEach(([genre, play]) => {
+    let count = 0;
+    for (let i = 0; i < sortedPlays.length; i++) {
+      const [index, info] = sortedPlays[i];
+      if (info.genre === genre) {
+        result.push(Number(index));
+        count++;
+      }
+      if (count === 2) {
+        break;
+      }
+    }
+  });
+
+  return result;
+};
+
+console.log(
+  solution(
+    ['classic', 'pop', 'classic', 'classic', 'pop'],
+    [500, 600, 150, 800, 2500]
+  )
+);
