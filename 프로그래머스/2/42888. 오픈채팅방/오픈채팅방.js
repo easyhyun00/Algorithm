cf13f21748439acfdbe25687cbe6dcf7e24f9ea9
이지현 <leejihyun0324@gmail.com>
2025-06-15 22:02:16 +0900

    [level 2] Title: 오픈채팅방, Time: 261.54 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,29 +1,4 @@
-/**
- * 오픈 채팅방
- * [https://school.programmers.co.kr/learn/courses/30/lessons/42888]
- */
-
-const solution = (record) => {
-  const answer = [];
-  const user = {};
-
-  // 유저 정보 저장
-  record.forEach((item) => {
-    const [action, id, name] = item.split(' ');
-    if (action === 'Enter' || action === 'Change') {
-      user[id] = name;
-    }
-  });
-
-  // 채팅 기록
-  record.forEach((item) => {
-    const [action, id] = item.split(' ');
-    if (action === 'Enter') {
-      answer.push(user[id] + '님이 들어왔습니다.');
-    } else if (action === 'Leave') {
-      answer.push(user[id] + '님이 나갔습니다.');
-    }
-  });
-
-  return answer;
-};
+function solution(record) {
+    var answer = [];
+    return answer;
+}
